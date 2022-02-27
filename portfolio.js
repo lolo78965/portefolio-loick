9fad2b71615d4774d78b1a25fdf0b53b00a568df
Loick Frobert <frobert.loick0@gmail.com>
2022-02-27 22:03:19 +0100

    loickportfolio

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,17 +11,16 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
+   
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -41,13 +40,13 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
+
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
